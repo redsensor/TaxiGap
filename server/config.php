4bab75152662b54939ec22b16747fd3738f18518
redsensor <freakrage@yandex.ru>
2014-04-27 20:48:14 +0300

    Update config.php

--- server/config.php
+++ server/config.php
@@ -1,8 +1,8 @@
 <?php
-$host='whirlwar.mysql.ukraine.com.ua'; 
-$database='whirlwar_test'; 
-$user='whirlwar_test'; 
-$pswd='vn9rhnph'; 
+$host=''; 
+$database=''; 
+$user=''; 
+$pswd=''; 
  
 $dbh = mysql_connect($host, $user, $pswd) or die("Не могу соединиться с MySQL.");
 mysql_select_db($database) or die("Не могу подключиться к базе.");
